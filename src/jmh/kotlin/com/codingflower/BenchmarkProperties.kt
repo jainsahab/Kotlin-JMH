c462aec7d4fe0f2fc18a3fc2a9603cee00b2779f
Prateek Jain <prateek.jain23995@yahoo.com>
2021-04-28 19:54:21 +0530

    Reduce warmup and measurement iterations

--- src/jmh/kotlin/com/codingflower/BenchmarkProperties.kt
+++ src/jmh/kotlin/com/codingflower/BenchmarkProperties.kt
@@ -5,8 +5,8 @@ import org.openjdk.jmh.annotations.Mode.*
 import java.util.concurrent.TimeUnit
 
 @BenchmarkMode(value = [AverageTime, SampleTime])
-@Warmup(iterations = 2)
-@Measurement(iterations = 3, batchSize = 1)
+@Warmup(iterations = 1)
+@Measurement(iterations = 2, batchSize = 1)
 @State(Scope.Thread)
 @Fork(value = 1, jvmArgs = ["-Xms2G", "-Xmx2G"])
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
